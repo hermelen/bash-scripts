a6f26f382385f225ba14a7bca8ef04c351c5663e
hermelen <hermelen.peris@gmail.com>
2024-03-07 10:53:50 +0100

    remove source perso.sh

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source ~/workspace/perso/bash-scripts/scripts/perso.sh
 source ~/workspace/perso/bash-scripts/scripts/arch_utils.sh
 source ~/workspace/perso/bash-scripts/scripts/fun_utils.sh
 source ~/workspace/perso/bash-scripts/scripts/win_utils.sh
